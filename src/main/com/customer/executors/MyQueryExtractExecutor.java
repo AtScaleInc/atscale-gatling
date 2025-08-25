3ce2a4635178019e95338410046d2f242a1628f5
stevehall <steve.hall@atscale.com>
2025-08-25 15:01:46 -0700

    Initial commit

---
+++ src/main/com/customer/executors/MyQueryExtractExecutor.java
@@ -0,0 +1,15 @@
+package com.customer.executors;
+
+import com.atscale.java.executors.QueryExtractExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class MyQueryExtractExecutor extends QueryExtractExecutor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueryExtractExecutor.class);
+
+    public static void main(String[] args) {
+        MyQueryExtractExecutor executor = new MyQueryExtractExecutor();
+        executor.execute();
+    }
+}
