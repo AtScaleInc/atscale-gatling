0abf24f3155e3c3bbf31ebc095909d4943085dab
stevehall <steve.hall@atscale.com>
2025-09-10 11:24:42 -0700

    Updates the project to use atscale-gatling-core version 1.1.  That version adds support for the AtScale Installer version.  I tested this project with the update to the parent project.  After having run the ConfigurationExecutor to install the Hive driver, there are no issues with regressions.

---
+++ src/main/executors/ConfigurationExecutor.java
@@ -0,0 +1,9 @@
+package executors;
+
+public class ConfigurationExecutor extends com.atscale.java.executors.ConfigurationExecutor {
+
+    public static void main(String[] args) {
+        ConfigurationExecutor executor = new ConfigurationExecutor();
+        executor.execute();
+    }
+}
