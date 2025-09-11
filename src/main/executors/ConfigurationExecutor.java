10bc5507805727886aa5a96a87155ade6778acba
stevehall <steve.hall@atscale.com>
2025-09-10 17:59:49 -0700

    Further updates the project to use atscale-gatling-core version 1.1.

--- src/main/executors/ConfigurationExecutor.java
+++
@@ -1,9 +0,0 @@
-package executors;
-
-public class ConfigurationExecutor extends com.atscale.java.executors.ConfigurationExecutor {
-
-    public static void main(String[] args) {
-        ConfigurationExecutor executor = new ConfigurationExecutor();
-        executor.execute();
-    }
-}
