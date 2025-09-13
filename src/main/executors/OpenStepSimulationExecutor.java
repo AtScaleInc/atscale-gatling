197d76e38884fbe4752f6759c6e40dbc7f689b02
stevehall <steve.hall@atscale.com>
2025-09-13 08:45:32 -0700

    ATSCALE-41101 Enables users to specify log file names for simulation task executions, enables users to specify whether to append or overwrite logs

--- src/main/executors/OpenStepSimulationExecutor.java
+++ src/main/executors/OpenStepSimulationExecutor.java
@@ -36,30 +36,61 @@ public class OpenStepSimulationExecutor extends SimulationExecutor<OpenStep> {
         t3InjectionSteps.add(new AtOnceUsersOpenInjectionStep(1));
         //t3InjectionSteps.add(new RampUsersPerSecOpenInjectionStep(1, 5, 1));
 
+        List<OpenStep> atOnceInjectionSteps = new ArrayList<>();
+        t1InjectionSteps.add(new AtOnceUsersOpenInjectionStep(1));
 
+
+        // Three example tasks for the Container Version. Uncomment tasks.add as needed.
         MavenTaskDto task1 = new MavenTaskDto("Internet Sales XMLA Simulation");
-        tasks.add(task1);
+        //tasks.add(task1);
         task1.setMavenCommand("gatling:test");
+        task1.setRunLogFileName("internet_sales_xmla.log");
+        task1.setLoggingAsAppend(true);
         task1.setSimulationClass("com.atscale.java.xmla.simulations.AtScaleXmlaOpenInjectionStepSimulation");
         task1.setRunDescription("Internet Sales XMLA Model Tests");
         task1.addGatlingProperty("atscale.model", "internet_sales");
         task1.addGatlingProperty("atscale.gatling.injection.steps", injectionStepsAsJson(t1InjectionSteps));
 
         MavenTaskDto task2 = new MavenTaskDto("Internet Sales JDBC Simulation");
-        tasks.add(task2);
+       // tasks.add(task2);
         task2.setMavenCommand("gatling:test");
+        task2.setRunLogFileName("internet_sales_jdbc.log");
+        task2.setLoggingAsAppend(true);
         task2.setSimulationClass("com.atscale.java.jdbc.simulations.AtScaleOpenInjectionStepSimulation");
         task2.setRunDescription("Internet Sales JDBC Model Tests");
         task2.addGatlingProperty("atscale.model", "internet_sales");
         task2.addGatlingProperty("atscale.gatling.injection.steps", injectionStepsAsJson(t2InjectionSteps));
 
         MavenTaskDto task3 = new MavenTaskDto("TPC-DS JDBC Simulation");
-        tasks.add(task3);
+        //tasks.add(task3);
         task3.setMavenCommand("gatling:test");
+        task3.setRunLogFileName("tpcds_benchmark_hive.log");
         task3.setSimulationClass("com.atscale.java.jdbc.simulations.AtScaleOpenInjectionStepSimulation");
         task3.setRunDescription("TPCDS JDBC Model Tests");
         task3.addGatlingProperty("atscale.model", "tpcds_benchmark_model");
         task3.addGatlingProperty("atscale.gatling.injection.steps", injectionStepsAsJson(t3InjectionSteps));
+
+        // Two example tasks for the Installer Version. Exclude by removing tasks.add as needed.
+        MavenTaskDto task4 = new MavenTaskDto("Installer TPC-DS JDBC Simulation");
+        tasks.add(task4);
+        task4.setMavenCommand("gatling:test");
+        task4.setRunLogFileName("tpcds_benchmark_hive.log");
+        task4.setLoggingAsAppend(true);
+        task4.setSimulationClass("com.atscale.java.jdbc.simulations.AtScaleOpenInjectionStepSimulation");
+        task4.setRunDescription("TPCDS JDBC Model Tests");
+        task4.addGatlingProperty("atscale.model", "TPC-DS Benchmark Model");
+        task4.addGatlingProperty("atscale.gatling.injection.steps", injectionStepsAsJson(atOnceInjectionSteps));
+
+        MavenTaskDto task5 = new MavenTaskDto("Installer TPC-DS XMLA Simulation");
+        tasks.add(task5);
+        task5.setMavenCommand("gatling:test");
+        task5.setRunLogFileName("tpcds_benchmark_xmla.log");
+        task5.setLoggingAsAppend(false);
+        task5.setSimulationClass("com.atscale.java.xmla.simulations.AtScaleXmlaOpenInjectionStepSimulation");
+        task5.setRunDescription("TPCDS XMLA Model Tests");
+        task5.addGatlingProperty("atscale.model", "TPC-DS Benchmark Model");
+        task5.addGatlingProperty("atscale.gatling.injection.steps", injectionStepsAsJson(atOnceInjectionSteps));
+
         return tasks;
     }
 
