877c8a1e42dc5da9a3f29e5dde46ea87916b60a4
David Mariani <david.mariani@atscale.com>
2025-10-16 14:18:06 -0700

    updated snowflake regression test scripts and added pass/fail script

--- scripts/Snowflake/Step 5 - Regression Test for Performance Tolerance.sql
+++ scripts/Snowflake/Step 5 - Regression Test for Performance Tolerance.sql
@@ -1,8 +1,8 @@
 -- Regression Test 3: Compare performance within a threshold
 --
 -- Set the two run_keys you want to compare
-set run_key_a = 'INSERT RUN KEY A HERE';
-set run_key_b = 'INSERT RUN KEY B HERE';
+set gatling_run_id_a = 'TPC-DS JDBC Model Tests | 1 Users | 2025-10-16-09:01:06';
+set gatling_run_id_b = 'TPC-DS JDBC Model Tests | 1 Users | 2025-10-16-09:01:06';
 
 -- Set your performance threshold as a percent (e.g. 10 = 10%)
 set perf_threshold_pct = 10;
@@ -14,11 +14,13 @@ with
             test_name,
             model as model_name,
             query_name,
+            query_hash,
+            status,
             concurrent_users,
             header_duration_ms as duration_ms
         from v_gatling_joined
-        where run_key = $run_key_a
-    qualify row_number() over (partition by test_name, model, query_name, concurrent_users order by header_ts desc) = 1
+        where gatling_run_id = $gatling_run_id_a
+    qualify row_number() over (partition by test_name, model, query_hash, concurrent_users order by header_ts desc) = 1
     ),
     b as (
 select
@@ -26,11 +28,13 @@ select
     test_name,
     model as model_name,
     query_name,
+    query_hash,
+    status,
     concurrent_users,
     header_duration_ms as duration_ms
 from v_gatling_joined
-where run_key = $run_key_b
-    qualify row_number() over (partition by test_name, model, query_name, concurrent_users order by header_ts desc) = 1
+where gatling_run_id = $gatling_run_id_b
+    qualify row_number() over (partition by test_name, model, query_hash, concurrent_users order by header_ts desc) = 1
     ),
 
     joined as (
@@ -38,6 +42,8 @@ select
     a.test_name,
     a.model_name,
     a.query_name,
+    a.query_hash,
+    a.status,
     a.concurrent_users,
     a.duration_ms as duration_a,
     b.duration_ms as duration_b,
@@ -46,7 +52,7 @@ from a
     join b
 on a.test_name        = b.test_name
     and a.model_name       = b.model_name
-    and a.query_name       = b.query_name
+    and a.query_hash       = b.query_hash
     and a.concurrent_users = b.concurrent_users
     )
 
@@ -54,6 +60,8 @@ select
     test_name,
     model_name,
     query_name,
+    query_hash,
+    status,
     concurrent_users,
     duration_a,
     duration_b,
@@ -65,4 +73,4 @@ select
         end as perf_change
 from joined
 where abs(pct_diff) >= $perf_threshold_pct
-order by abs(pct_diff) desc, test_name, query_name;
+order by abs(pct_diff) desc, test_name, query_hash;
--- scripts/Step 3 - Regression Test for Query Pass-Fail.sql
+++
@@ -1,7 +0,0 @@
--- Regression Test 1: Look for failing queries
---
--- Set the two run keys you want to compare
-set run_key_a = 'INSERT RUN KEY A HERE';
-set run_key_b = 'INSERT RUN KEY B HERE';
-
--- TODO: Need to have pass or fail in run_logs as a field
